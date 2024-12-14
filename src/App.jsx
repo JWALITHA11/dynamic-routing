0895d05360c3556135addc80ce276e3fea416231
JWALITHA11 <jwalithamarella@gmail.com>
2024-12-14 11:50:16 +0530

    new changes added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,8 +16,11 @@ function App() {
           <li>contact us</li>
         </ul>
       </div>
+      <div className='App'>
+        <Navbar />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
